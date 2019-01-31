28983a36eb0cbe42cef479de2d42c273b0bbb2e0
Mallory Woods <malvwoods@gmail.com>
2019-01-31 17:34:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var height = "74"
 
 if (name === "Susan") {
   console.log("The name is correct")
-}
-else (name === "Joe") {
+} else (name === "Joe") {
   console.log("Expected the name to be Susan")
 }
+
 
 // Don't worry about this
 module.exports = { name, height
